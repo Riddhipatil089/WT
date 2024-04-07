f8be5dbc6888b752be04fa2f873f776152921f27
Riddhi Kishor Patil <166218064+Riddhipatil089@users.noreply.github.com>
2024-04-07 15:12:44 +0530

    Pyramid_66

---
+++ pyramid.php
@@ -0,0 +1,23 @@
+<html> 
+    <head>
+        <title> Pyramid format using star's.</title>
+    </head>
+    <body> 
+        <?php
+       
+$size = 9;
+for($i=1;$i<=$size;$i++)
+{
+    for($j=1;$j<=$size-$i;$j++)
+    {
+        echo "&nbsp;&nbsp;";
+    }
+    for($k=1;$k<=$i;$k++)
+    {
+                echo "*&nbsp;&nbsp;";
+    }
+echo "<br/>";
+}
+?>
+</body>
+</html>
